1280f1bd741235bc0d5f34039c48071ccd2abd37
Mushfekur Rahman <mushfekur@therapservices.net>
2016-01-05 13:21:46 +0600

    Initial project structure

---
+++ src/main/java/com/mushfek/xmlparser/Main.java
@@ -0,0 +1,11 @@
+package com.mushfek.xmlparser;
+
+/**
+ * @author Mushfekur Rahman
+ * @since 1.0-SNAPSHOT
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
